e554a04731443f0330f0159a75c2fa625a2dbb98
ShimanNutsu <nikolyashiman@gmail.com>
2023-10-16 18:57:51 +0300

    update peg

--- embodied/agents/go_explore/peg.py
+++ embodied/agents/go_explore/peg.py
@@ -15,6 +15,7 @@ from . import goal_picker
 # import worker
 from . import behaviors
 
+# TODO: make go-explore the agent itself in agent.py, not task_behavior
 class PEG(tfutils.Module):
 
     def __init__(self, wm, act_space, config):
@@ -23,13 +24,17 @@ class PEG(tfutils.Module):
         self.config = config
         
         self.explorer = behaviors.Explore(self.wm, self.act_space, self.config)
-        wconfig = config.update({
-                'actor.inputs': self.config.worker_inputs,
-                'critic.inputs': self.config.worker_inputs,
-        })
-        self.worker = agent.ImagActorCritic({
-            'goal': agent.VFunction(lambda s: self.goal_reward(s), wconfig),
-        }, config.worker_rews, act_space, wconfig)
+        if config.worker == 'ac':
+            wconfig = config.update({
+                    'actor.inputs': self.config.worker_inputs,
+                    'critic.inputs': self.config.worker_inputs,
+            })
+            self.worker = agent.ImagActorCritic({
+                'goal': agent.VFunction(lambda s: self.goal_reward(s), wconfig),
+            }, config.worker_rews, act_space, wconfig)
+        elif config.worker == 'gcsl':
+            self.worker = agent.ImagGCSL(act_space, wm, config)
+        
 
         goal_picker_cls = getattr(goal_picker, config.goal_strategy)
         p_cfg = config.planner
@@ -53,14 +58,14 @@ class PEG(tfutils.Module):
             goal_strategy = goal_picker_cls(
                 self.wm,
                 self.worker.actor,
-                self.explorer.ac.critics['expl'].reward_fn,
-                value_fn=self.explorer.ac.critics['expl'].target,
-                gc_input=config.gc_input,
+                #self.explorer.ac.critics['disag'].rewfn,
+                self.explorer.ac.critics['disag'].target,
+                gc_input='embed',
                 goal_dim=2,
                 goal_min=np.array(p_cfg.goal_min, dtype=np.float32),
                 goal_max=np.array(p_cfg.goal_max, dtype=np.float32),
                 act_space=self.act_space,
-                state_key=config.state_key,
+                state_key='observation',
                 planner=p_cfg.planner_type,
                 horizon=p_cfg.horizon,
                 batch=p_cfg.batch,
@@ -83,7 +88,7 @@ class PEG(tfutils.Module):
             raise NotImplementedError
         
         self.goal_picker = goal_strategy
-        self.goal_shape = self.goal_picker.goal_shape
+        self.goal_shape = (self.config.encoder.mlp_units, )
     
     def policy(self, latent, carry, obs, mode='train'):
         sg = lambda x: tf.nest.map_structure(tf.stop_gradient, x)
@@ -95,13 +100,17 @@ class PEG(tfutils.Module):
             goal = goal_embed
             obs = sg({**latent, 'goal': goal})
             outs, _ = self.worker.policy(obs, carry)
-            carry = {'step': carry['step'] + 1, 'goal': goal}
+            carry = {'step': carry['step'] + 1, 'goal': goal, 'real_goal': carry['real_goal']}
             return outs, carry
 
         update = (obs['is_first'] == 1).all()
+        real_goal = carry['real_goal']
         if (carry['step'] % self.config.env.length < self.config.gc_duration).all():
             if update.all():
-                goal = self.goal_picker.policy(latent, carry)
+                obs.pop('goal', None)
+                real_goal = self.goal_picker.policy(latent, carry)
+                goal_obs = {'observation': real_goal}
+                goal = self.wm.encoder(goal_obs)
             else:
                 goal = carry['goal']
             actor = self.worker
@@ -112,7 +121,7 @@ class PEG(tfutils.Module):
             actor = self.explorer
             _obs = sg(latent)
             outs, _ = actor.policy(_obs, carry)
-        carry = {'step': carry['step'] + 1, 'goal': goal}
+        carry = {'step': carry['step'] + 1, 'goal': goal, 'real_goal': real_goal}
         if (obs['is_last'] == 1).all():
             carry['step'] = tf.zeros(carry['step'].shape, tf.int64)
         return outs, carry
@@ -120,19 +129,30 @@ class PEG(tfutils.Module):
     def initial(self, batch_size):
         return {
                 'step': tf.zeros((batch_size,), tf.int64),
-                'goal': tf.zeros((batch_size,) + self.goal_shape, tf.float32),
+                'goal': tf.zeros((batch_size,) + self.goal_shape, tf.float16),
+                'real_goal': tf.zeros((batch_size,) + (2, ), tf.float16),
         }
     
     def train(self, imagine, start, data):
         metrics = {}
-        real_goal = start['observation']
-        goal_embed = self.wm.encoder(data)
-        goal_embed = goal_embed.reshape([-1] + list(goal_embed.shape[2:]))
-        ids = tf.random.shuffle(tf.range(tf.shape(goal_embed)[0]))
-        goal_embed = tf.gather(goal_embed, ids)
-        real_goal = tf.gather(real_goal, ids)
-        start['goal'] = goal_embed
-        start['real_goal'] = real_goal
+        if self.config.train_goal == 'batch':
+            real_goal = start['observation']
+            goal_embed = self.wm.encoder(data)
+            goal_embed = goal_embed.reshape([-1] + list(goal_embed.shape[2:]))
+            ids = tf.random.shuffle(tf.range(tf.shape(goal_embed)[0]))
+            goal_embed = tf.gather(goal_embed, ids)
+            real_goal = tf.gather(real_goal, ids)
+            start['goal'] = goal_embed
+            start['real_goal'] = real_goal
+        elif self.config.train_goal == 'traj':
+            print('---------------')
+            real_goal = data['real_goal']
+            real_goal = real_goal.reshape([-1] + list(real_goal.shape[2:]))
+            goal_obs = {'observation': real_goal}
+            goal = self.wm.encoder(goal_obs)
+            start['goal'] = goal
+            start['real_goal'] = real_goal
+
         _, mets = self.worker.train(self.wm.imagine, start, data)
         metrics.update({f'worker_{k}': v for k, v in mets.items()})
         _, mets = self.explorer.train(self.wm.imagine, start, data)
@@ -154,5 +174,15 @@ class PEG(tfutils.Module):
         elif self.config.goal_reward == 'euclid':
             goal = tf.stop_gradient(traj['real_goal'].astype(tf.float32))
             feat = self.wm.heads['decoder'](traj)['observation'].mode().astype(tf.float32)
+            # print(goal.max(), feat.max(), sep='==========')
+            # print(goal.min(), feat.min(), sep='==========')
+            # print(tf.math.sqrt(tf.square(goal-feat).sum(axis=2))[1:].max())
             goal = goal.reshape(feat.shape)
+            return -tf.math.sqrt(tf.square(goal-feat).sum(axis=2))[1:]
             return tf.math.reduce_euclidean_norm(goal - feat, axis=2)[1:]
+        elif self.config.goal_reward == '0_1':
+            goal = tf.stop_gradient(traj['real_goal'].astype(tf.float32))
+            feat = self.wm.heads['decoder'](traj)['observation'].mode().astype(tf.float32)
+            dist = tf.math.sqrt(tf.square(goal-feat).sum(axis=2))[1:]
+            reward = (-tf.math.sign(dist - 0.4) + 1) / 2
+            return tf.stop_gradient(reward)
